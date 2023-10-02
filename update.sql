ae198d02bf69f1c9556197dc58d66c110dfb34ab
Qingyun Wang <lucas.qingyun@gmail.com>
2023-10-02 22:38:24 +0300

    add aircraft table insert 10 records data need to be changed

--- update.sql
+++ update.sql
@@ -4,8 +4,29 @@ drop table goal_reached;
 drop table game;
 
 create table user(
-ID int not null auto_increment,
+id int not null auto_increment,
 name varchar(40),
 password varchar(40),
 primary key (id)
 );
+
+create table aircraft(
+    id int not null auto_increment,
+    name varchar(255) not null,
+    passenger_capacity int not null,
+    flight_range int not null,
+    price int not null,
+    carbon_emission int not null,
+    PRIMARY KEY (id)
+);
+
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Basic plane', 50, 500, 0, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Advanced plane', 100, 1000, 10000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Eco plane', 60, 800, 7000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Super plane', 150, 1500, 15000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Ultimate plane', 200, 2000, 50000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Turbo jet', 250, 3000, 60000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Eco jet', 180, 2200, 55000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Luxury liner', 300, 3500, 75000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Cargo plane', 20, 1500, 20000, 1);
+insert into aircraft (name, passenger_capacity, flight_range, price, carbon_emission) values ('Stealth jet', 100, 2500, 65000, 1);
